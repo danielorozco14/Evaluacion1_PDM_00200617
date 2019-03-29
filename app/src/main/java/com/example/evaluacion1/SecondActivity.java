468e96c98313b637f0d857462e536cfa22d99f91
danielorozco14 <00200617@uca.edu.sv>
2019-03-29 15:29:11 -0600

    Falta setear valores en second activity

--- app/src/main/java/com/example/evaluacion1/SecondActivity.java
+++ app/src/main/java/com/example/evaluacion1/SecondActivity.java
@@ -16,6 +16,7 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         getID();
+        setTotales();
     }
 
     public void getID(){
